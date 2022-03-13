dcf454aa6c1eefb7de684185f2f15bf9bdcbeb85
Selim-Dev <alisleem811@gmail.com>
2022-03-14 00:57:04 +0200

    new tours

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-var cors = require('cors');
-
+const cors = require('cors');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const path = require('path');
@@ -11,10 +10,6 @@ const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const app = express();
 const morgan = require('morgan');
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-const reviewRouter = require('./routes/reviewRoutes');
-const viewRouter = require('./routes/viewRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -63,7 +58,7 @@ app.use(
     })
 );
 // Serving Static Files
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Test Middleware
 app.use((req, res, next) => {
@@ -73,10 +68,7 @@ app.use((req, res, next) => {
 });
 
 /****************** 2) routes ******************************/
-app.use('/', viewRouter);
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/reviews', reviewRouter);
+
 // app.get('/api/v1/tours', getAllTours);
 // app.post('/api/v1/tours', createNewTour);
 
