e7692791f1dca945b90adedde2da7e608c45f6e6
Selim-Dev <alisleem811@gmail.com>
2022-03-24 01:39:19 +0200

    test

--- models/OrderItem.js
+++ models/OrderItem.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 // const SubCategory = require('./SubCategory');
 const OrderItemSchema = new mongoose.Schema({
-    test: { type: String },
     name: {
         type: String,
         required: true
