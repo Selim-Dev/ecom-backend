dafb1e46060927c9cca1437f5ee0654032babf98
Selim-Dev <alisleem811@gmail.com>
2022-03-25 15:57:42 +0200

    after merging variatns

--- validations/categoryJoi.js
+++ validations/categoryJoi.js
@@ -1,15 +1,11 @@
 const joi = require('joi');
 
 exports.categoryJoi = (category) => {
-
     const schema = joi.object({
         name: joi.string().alphanum().required(),
-        photo: joi.string().alphanum().required(),
+        photo: joi.string().alphanum().required()
     });
     const validationResult = schema.validate(category);
 
-
-    return validationResult.error
-
+    return validationResult.error;
 };
-
